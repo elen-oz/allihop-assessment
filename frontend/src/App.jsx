47aa0155f5253f472d407a93ef3b2f55dd2a8e2e
Elena Ozerova <ozerova.lenka@gmail.com>
2023-04-03 23:03:51 +0200

    feat: add func render countries list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const filterCountriesByName = (countries, search) => countries.filter((country) => country.name.common.toLowerCase().includes(search.toLowerCase()));
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -12,6 +13,64 @@ function App() {
       .then((response) => setCountries(response.data))
       .catch((error) => console.log(error));
   }, []);
+
+  const renderCountries = () => {
+    if (filteredCountries.length === 0) {
+      return <p>No countries found.</p>;
+    }
+    if (filteredCountries.length === 1) {
+      const country = filteredCountries[0];
+      return (
+        <div>
+          <h2>{country.name.common}</h2>
+          <p>
+            Capital:
+            {' '}
+            {country.capital}
+          </p>
+          <p>
+            Population:
+            {' '}
+            {formatNumber(country.population)}
+          </p>
+          <img src={country.flags.png} alt={country.name.common} width="300" />
+        </div>
+      );
+    }
+    if (filteredCountries.length <= 10) {
+      return filteredCountries.map((country) => (
+        <div key={country.cca3}>
+          <h2>{country.name.common}</h2>
+          <p>
+            Capital:
+            {' '}
+            {country.capital}
+          </p>
+          <p>
+            Population:
+            {' '}
+            {formatNumber(country.population)}
+          </p>
+          <img src={country.flags.png} alt={country.name.common} width="50" />
+        </div>
+      ));
+    }
+    return <p>Too many matches, please specify another filter.</p>;
+  };
+
+  return (
+    <div>
+      <h1>Countries</h1>
+      <div>
+        <label htmlFor="country-input">
+          Find countries:
+          {' '}
+          <input type="text" value={search} id="country-input" onChange={handleSearchChange} />
+        </label>
+      </div>
+      {renderCountries()}
+    </div>
+  );
 }
 
 export default App;
